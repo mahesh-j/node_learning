4d1ff9ca7e2e17bd8a343eb1305b981a277e3d27
Mahesh Jagannath <mahesh.jagannath@UTG.local>
2018-06-29 14:48:09 +0530

    Basic Initial

---
+++ basic/app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>Hello World</h1>');
+    res.write('</body>');
+    res.end('</html>');
+});
+
+server.listen(port, hostname,() => {
+    console.log(`Server is running at http://${hostname}:${port}`);
+});
---
+++ basic/event_emitter.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', (a, b) => {
+    console.log(a, b, this);
+});
+
+myEmitter.emit('event', 'This is a', 'This is b');
